868a536f8001bd4a8e05115ac67b2d1f6921963e
Adrian Wanjau <adrianhubba@gmail.com>
2020-03-02 08:55:20 +0300

    Designed delivery section

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
+// business logic
+function Order(size, crust, toppings) {
+    this.pizzaSize = size;
+    this.crustType = crust;
+    this.pizzaToppings = toppings;
+}
+
+Order.prototype.pizzadetails = function() {
+    return this.pizzaSize + " " + this.crustType + " " + this.pizzaToppings;
+}
+
+function Contact(first, last, place) {
+    this.firstName = first;
+    this.lastName = last;
+}
+
+
+Contact.prototype.fullName = function() {
+    return this.firstName + " " + this.lastName;
+}
+
+  
+// user interface logic
 $(document).ready(function() {
     $(".clickable1").click(function() {
         $(".columnshowing").toggle();
@@ -10,5 +33,26 @@ $(document).ready(function() {
     $(".clickable3").click(function() {
         $(".columnshowing3").toggle();
         $(".columnone3 img").toggle();
+    });
+    $(".row").click(function() {
+        $(".hidden-delivery").toggle();
+        $(".show-delivery").toggle();
+    });
+    
+    
+    $(".form").submit(function(event) {
+        event.preventDefault();
+    
+        var inputtedSize = $("radio#size").val();
+        var inputtedCrust = $("radio#crust").val();
+        var inputtedTopping = $("radio#topping").val();
+    
+        var newOrder = new Order(inputtedSize, inputtedCrust, inputtedTopping);
+    
+        $("ul#Order").append("<li><span class='order'>" + newOrder.pizzadetails() + "</span></li>");
+    
+        $("radio#size").val();
+        $("radio#crust").val();
+        $("radio#topping").val();
     });
 });
